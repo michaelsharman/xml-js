267920b23f4725acdf13d577f65b24f691db676d
Michael Sharman <sharmo@gmail.com>
2024-03-13 16:08:01 +1100

    Added ES dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,20 @@
-const path = require('path');
 const webpack = require('webpack');
 const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
 module.exports = {
-  entry: {
-    'dist': './lib/index.js',
-    'doc': './lib/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, '.'),
-    filename: '[name]/xml-js.min.js',
-    libraryTarget: 'window',
-    // library: 'xmljs' // don't specify this
-  },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.(js)$/,
-  //       use: 'babel-loader'
-  //     }
-  //   ]
-  // },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new UnminifiedWebpackPlugin()
-  ]
-}
+    entry: {
+        dist: './lib/index.js',
+        doc: './lib/index.js',
+    },
+    output: {
+        path: __dirname + '/',
+        filename: '[name]/xml-js.min.js',
+        libraryTarget: 'window',
+    },
+    plugins: [new UnminifiedWebpackPlugin()],
+    resolve: {
+        fallback: {
+            stream: require.resolve('stream-browserify'),
+        },
+    },
+};
